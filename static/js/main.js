e54d3ab5e86a4835980b127cc8365dd4f15456d8
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2021-07-24 21:46:51 +0100

    dom testing

--- static/js/main.js
+++ static/js/main.js
@@ -1,33 +1,7 @@
-console.log("hello");
+const helpers = require("./helpers");
 
 const loginButton = document.querySelector("#login-button");
 const signInButton = document.querySelector("#sign-up-button");
 
-
-
-function setActiveButton(e) {
-    // See is the button which called the event already active.
-    // if it is do nothing.
-    
-    if (e.target.classList.contains("active")) {
-        // this one is already live so no need to do anything.
-        return;
-    }
-    
-    loginButton.classList.toggle("active");
-    signInButton.classList.toggle("active");
-
-    const formHeading = document.getElementById("form-heading");
-    const submitButton = document.getElementById("submit-button");
-
-    if (formHeading.textContent === "log in.") {
-        formHeading.textContent = "sign up.";
-        submitButton.value = "sign up";
-    } else {
-        formHeading.textContent = "log in.";
-        submitButton.value = "log in";
-    };  
-}
-
-loginButton.addEventListener("click", setActiveButton);
-signInButton.addEventListener("click", setActiveButton);
+loginButton.addEventListener("click", helpers.setActiveButton);
+signInButton.addEventListener("click", helpers.setActiveButton);
